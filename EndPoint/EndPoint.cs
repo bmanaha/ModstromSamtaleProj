4eb3a0e743aca001cf43005e43dfe631984d73e6
Bman <>
2021-01-06 18:52:06 +0100

    EndPoint.CS tracked again

---
+++ EndPoint/EndPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2samtopg.Endpoint
+{
+    public class EndPoint
+    {
+
+        public static readonly Uri uri = new Uri
+        (//Do not push to Git
+            ""
+        );
+    }
+}
